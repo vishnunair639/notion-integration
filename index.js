64db6fddbcf2764a737752fb25f3ed90f4ffce34
Vishnu <25422446+vishnunair639@users.noreply.github.com>
2021-10-19 00:04:41 +0530

    🔒️ Deleting unused code

--- index.js
+++ index.js
@@ -13,12 +13,5 @@ const listDatabases = async() =>{
 }
 //listDatabases();
 const database_id = process.env.NOTION_DATABASE_ID
-const getSvelteResources = async() =>{
-    const payload = {
-        path: `databases/08546093-ed5f-4fff-b41b-beddb10f1230`,
-        method: 'POST'
-    }
-    const results = await notion.databases.retrieve(payload);
-    console.log("Svelte Resources from DB:", results);
-};
+
 listDatabases();
